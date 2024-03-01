ca3e44032442c158d7aa9b6c31a6691a85780ca2
H82M19\Student <Student>
2024-03-01 11:20:01 +0700

    commit handle save

---
+++ data.js
@@ -0,0 +1,24 @@
+const courses = [
+    {
+        id: '1',
+        name: 'Cơ sở dữ liệu',
+        course_type: 'Cơ sở',
+        semester: 'HK1-2020-2021',
+        department: 'K.CNTT'
+    },
+    {
+        id: 2,
+        name: 'Cấu trúc dữ liệu',
+        course_type: 'Cơ sở',
+        semester: 'HK1-2020-2021',
+        department: 'K.CNTT'
+    },
+    {
+        id: 3,
+        name: 'Công nghệ phần mềm',
+        course_type: 'Cơ sở',
+        semester: 'HK1-2020-2021',
+        department: 'K.CNTT'
+    }
+];
+module.exports = courses;
